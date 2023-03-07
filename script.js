f3fd9147385ee75bd46885070d0ca57e7a080ffa
Renkensg <renkensg@gmail.com>
2023-03-07 12:44:29 -0300

    fixing

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const card = document.querySelector('.card');
 const frontTextHover = document.querySelector('.front__text-hover');
 
 // Verifica se o dispositivo é móvel
@@ -6,15 +5,5 @@ const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
 // Adiciona o texto e o listener de evento de acordo com o tipo de dispositivo
 if (isMobile) {
-  card.addEventListener('click', function () {
-    this.classList.toggle('is-flipped');
-  });
   frontTextHover.textContent = 'Toque para me encontrar';
-} else {
-  card.addEventListener('mouseenter', function () {
-    this.classList.add('is-flipped');
-  });
-  card.addEventListener('mouseleave', function () {
-    this.classList.remove('is-flipped');
-  });
 }
