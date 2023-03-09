94154561aa9940ffdec8f222aafe82e443407eea
Renkensg <renkensg@gmail.com>
2023-03-09 10:50:36 -0300

    Change to english

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
 // Adiciona o texto e o listener de evento de acordo com o tipo de dispositivo
 if (isMobile) {
-  frontTextHover.textContent = 'Toque para me encontrar';
+  frontTextHover.textContent = 'Touch to find me';
 }
 
 function rotateInnerDiv() {
